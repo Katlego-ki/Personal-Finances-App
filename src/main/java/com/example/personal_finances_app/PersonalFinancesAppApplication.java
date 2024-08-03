4a38d195940ebc2922fe1f9687ceba0a772bf9e9
Katlego-ki <katlegobairvin@gmail.com>
2024-08-03 14:14:11 +0200

    initial commit

---
+++ src/main/java/com/example/personal_finances_app/PersonalFinancesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.personal_finances_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personal_finances_app/PersonalFinancesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal_finances_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
